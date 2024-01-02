3a6dd9e6cf9448407661930e663daa325069a6e2
vanshika sanan <sanan.vanshika@gmail.com>
2024-01-02 21:03:27 +0530

    Updated The tree styles

--- src/data/index.js
+++ src/data/index.js
@@ -5,96 +5,66 @@ export default [
     team: "Executive Team",
     children: [
       {
-        name: "Dennis Rice",
-        role: "CTO",
-        team: "Technology",
+        name: "Emily Turner",
+        role: "Director Engineering",
+        team: "Engineering",
         children: [
           {
-            name: "Emily Turner",
-            role: "Director Engineering",
+            name: "Samuel Carter",
+            role: "Engineering Manager",
             team: "Engineering",
             children: [
               {
-                name: "Samuel Carter",
-                role: "Engineering Manager",
+                name: "Olivia Davis",
+                role: "Senior Software Engineer",
+                team: "Engineering",
+              },
+              {
+                name: "Lucas Evans",
+                role: "Senior Software Engineer",
                 team: "Engineering",
                 children: [
                   {
-                    name: "Olivia Davis",
-                    role: "Senior Software Engineer",
+                    name: "Liam Thompson",
+                    role: "Software Engineer",
                     team: "Engineering",
                   },
                   {
-                    name: "Lucas Evans",
-                    role: "Senior Software Engineer",
+                    name: "Ava Martinez",
+                    role: "Software Engineer",
                     team: "Engineering",
-                    children: [
-                      {
-                        name: "Liam Thompson",
-                        role: "Software Engineer",
-                        team: "Engineering",
-                      },
-                      {
-                        name: "Ava Martinez",
-                        role: "Software Engineer",
-                        team: "Engineering",
-                      },
-                    ],
-                  },
-                ],
-              },
-              {
-                name: "Sophia White",
-                role: "Director of IT Infrastructure",
-                team: "IT",
-                children: [
-                  {
-                    name: "Aiden Harris",
-                    role: "Network Manager",
-                    team: "IT",
-                    children: [
-                      {
-                        name: "Emma Foster",
-                        role: "Network Engineer",
-                        team: "IT",
-                      },
-                      {
-                        name: "Henry Adams",
-                        role: "Network Engineer",
-                        team: "IT",
-                      },
-                    ],
                   },
                 ],
               },
             ],
           },
-          // {
-          //   name: "Alexandra Johnson",
-          //   role: "Director of Marketing",
-          //   team: "Marketing",
-          //   children: [
-          //     {
-          //       name: "Michael Williams",
-          //       role: "Marketing Manager",
-          //       team: "Marketing",
-          //       children: [
-          //         {
-          //           name: "Sophie Brown",
-          //           role: "Marketing Specialist",
-          //           team: "Marketing",
-          //         },
-          //         {
-          //           name: "David Wilson",
-          //           role: "Marketing Specialist",
-          //           team: "Marketing",
-          //         },
-          //       ],
-          //     },
-          //   ],
-          // },
+          {
+            name: "Sophia White",
+            role: "Director of IT Infrastructure",
+            team: "IT",
+            children: [
+              {
+                name: "Aiden Harris",
+                role: "Network Manager",
+                team: "IT",
+                children: [
+                  {
+                    name: "Emma Foster",
+                    role: "Network Engineer",
+                    team: "IT",
+                  },
+                  {
+                    name: "Henry Adams",
+                    role: "Network Engineer",
+                    team: "IT",
+                  },
+                ],
+              },
+            ],
+          },
         ],
       },
+      ,
     ],
   },
 ];
